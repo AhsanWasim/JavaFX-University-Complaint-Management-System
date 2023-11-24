80d8fd2c9400a61dd1ee87fd4b83cdf2fc193d59
Ihtisham-Alii <96953609+Ihtisham-Alii@users.noreply.github.com>
2023-11-25 02:09:20 +0500

    Started Login Page

--- Login.java
+++ Login.java
@@ -1,4 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class Login {
 
+public class Login extends JFrame {
+
+	Login()
+	{
+		setLayout(null);
+		setTitle("Complaint Management System");
+		
+		ImageIcon IC1= new ImageIcon(ClassLoader.getSystemResource("icons/ii.jpeg"));
+		Image I2=IC1.getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT);
+		ImageIcon re= new ImageIcon(I2);
+		JLabel labl1= new JLabel(re);
+		labl1.setBounds(50,40,200,200);
+		add(labl1);
+		
+		JLabel show= new JLabel("Welcome to Login Page");
+		show.setFont(new Font("Osward",Font.BOLD,25));
+		show.setBounds(250,100,400,100);
+		add(show);
+		//labl1.setVisible(true);
+		getContentPane().setBackground(Color.GRAY);
+		
+		setSize(800,480);
+		setVisible(true);
+		setLocation(350,150);
+	}
+	
+	
 }
--- main.java
+++ main.java
@@ -1,11 +1,8 @@
 
 
-public class Main {
+public class Main  {
 	public static void main(String[] args) {
-		System.out.print("HELLO!");
-		System.out.print("HELLO!");
-		System.out.print("HELLO!");
-		System.out.print("HELLO!");
 		
+		new Login();
 	}
 }
